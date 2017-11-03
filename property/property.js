7ea7614f1d0b49726d1e01af94234ded18cf6524
Medinilla <cris.medinill@gmail.com>
2017-11-03 13:34:16 -0500

    Add selector helper (#2)

--- property/property.js
+++ property/property.js
@@ -1,15 +1,23 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
+import { filterSelectorByAttribute } from '../helpers/selectors';
 
-fixture `Getting Started`// declare the fixture
-    .page `https://devexpress.github.io/testcafe/example`;  // specify the start page
+fixture `property.property`// declare the fixture
+    .page `http://localhost:3000/fire/properties/#/property/c08f47df-1fc0-e711-9be3-e4a471db8629/property`;  // specify the start page
 
+const infoTypeSelectButton = filterSelectorByAttribute('eso-single-select','with-address-from','vm.info.typeId');
 
 //then create a test and place your code there
-test('My first test', async t => {
+test('info.type', async t => {
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button')
+        .click(infoTypeSelectButton)
+        .click('.label-content')
 
         // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        .expect(Selector('.display-value').innerText).eql('Assembly');
 });
+
+
+
+
+
+
