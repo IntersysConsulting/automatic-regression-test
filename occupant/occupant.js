cb528a54c7e7410c341c568ea300e8d42c09fd41
Medinilla <cris.medinill@gmail.com>
2017-11-10 13:50:41 -0600

    Add text input test (#11)

--- occupant/occupant.js
+++ occupant/occupant.js
@@ -6,11 +6,13 @@ import { numberInput } from '../helpers/numberInput';
 import { timeSelect } from '../helpers/timeSelect';
 import { dateSelect } from '../helpers/dateSelect';
 import { yesNo } from '../helpers/yesNo';
+import { text } from '../helpers/text';
+
 
 const json = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 fixture`occupant.occupant`// declare the fixture
-  .page`${json.host}/occupant/7fa43d18-20c0-e711-9be3-e4a471db8629/occupant`; 
+  .page`${json.host}/occupant/${json.occupantUID}/occupant`; 
 
 const otherTab = filterSelectorByAttribute('li','ui-sref','occupant.contacts');
 const occupantTab = filterSelectorByAttribute('li','ui-sref','occupant.occupant');
@@ -20,7 +22,7 @@ const occupantTab = filterSelectorByAttribute('li','ui-sref','occupant.occupant'
 
 //info.occupantName
 const occupantNameExpectedValue = 'Occupant';
-const occupantNameSelectButton  = Selector('.shelf-click-indicator').withText('Change Occupant Name/Number');
+const occupantNameSelectButton  = Selector('.shelf-click-indicator').withText('Change Occupant Name');
 const occupantNameType  = filterSelectorByAttribute('input','type','text');
 const occupantNameOk = Selector('.btn').withText("Change Name");
 
@@ -50,7 +52,7 @@ yesNo('vm.info.doesSecurityCarryKeys',occupantTab,otherTab);
 yesNo('vm.permits.isOccupancyPermitRequired',occupantTab,otherTab);
 singleSelect('Mercantile: Group M', 'vm.permits.nfpaUseCodeId', occupantTab, otherTab);
 singleSelect('Educational', 'vm.permits.nfpaOccCodeId', occupantTab, otherTab);
-dateSelect('10112017', '10/11/2017', 'OCCUPANT_CERTIFICATEOFOCCUPANCYDATEOFISSUE', buildingTab, otherTab);
+dateSelect('10112017', '10/11/2017', 'OCCUPANT_CERTIFICATEOFOCCUPANCYDATEOFISSUE', occupantTab, otherTab);
 //singleSelect('Private', 'vm.permits.stateOccupancyCodeId', occupantTab, otherTab);
 //singleSelect('Private', 'vm.permits.localOccupancyClassId', occupantTab, otherTab);
 yesNo('vm.permits.certificateOfOccupancyIssued',occupantTab,otherTab);
@@ -78,6 +80,10 @@ timeSelect('100000', '10:00:00', 'vm.businessOperation.openToPublicEnd', occupan
 numberInput('123456', 'vm.businessOperation.numberOfEmployees', 6,occupantTab, otherTab);
 yesNo('vm.businessOperation.hasEmergencyIncidentPlan',occupantTab,otherTab);
 
-// //MONITORING
+//MONITORING
+text('abcdefghi', 'vm.monitoring.extinguisherMonitoringCompany', 50,occupantTab, otherTab);
+text('abcdefghi', 'vm.monitoring.extinguisherMonitoringCompanyPhone', 50,occupantTab, otherTab);
+text('abcdefghi', 'vm.monitoring.alarmsMonitoringCompany', 50,occupantTab, otherTab);
+text('abcdefghi', 'vm.monitoring.alarmsMonitoringCompanyPhone', 50,occupantTab, otherTab);
 
 // //NOTES
